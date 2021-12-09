1317dc739a3265d64406f4d3a2c0844e13480d3e
vahehak93 <vahehak@gmail.com>
2021-12-09 13:35:48 +0400

    fix: valid_braces changed

--- valid_braces.js
+++ valid_braces.js
@@ -1,35 +1,47 @@
-let input = "[{}{}({()}){{{}}}]";
+let input = "{[({})]}{}[]()"; // []{()}(()){{[]}}{]
 let opens = "[{("
 let closes = "]})"
 let brk = 0;
 let sqr_br = 0;
 let crl_br = 0; 
-
+let error_count = 0;
+let count = 0;
 
 function valid_braces(input) {
 
     for (let i = 0; i < input.length; i++) {
-        if (input[i] === opens[1]) {
+        if (input[i] === opens[0] && input[i+1] !== closes[1] && input[i+1] !== closes[2]) {
             sqr_br -= 1
+            count++
         }
-        if (input[i] === closes[1]) {
+        if (input[i] === closes[0] && sqr_br < 0) {
             sqr_br += 1
-        }
-        if (input[i] === opens[2]) {
+            count++
+        }   
+        if (input[i] === opens[1] && input[i+1] !== closes[0] && input[i+1] !== closes[2]) {
             crl_br -= 1
+            count++
         }
-        if (input[i] === closes[2]) {
+        if (input[i] === closes[1] && crl_br < 0) {
             crl_br += 1
+            count++
+            
         }
-        if (input[i] === opens[3]) {
+        if (input[i] === opens[2] && input[i+1] !== closes[0] && input[i+1] !== closes[1]) {
             brk -= 1
+            count++
         }
-        if (input[i] === closes[3]) {
+        if (input[i] === closes[2] && brk < 0) {
             brk += 1
+            count++
         }
+        
     }
+    // console.log(input.length)
+    // console.log(count)
+    // console.log(error_count)
     console.log(sqr_br,crl_br,brk)
-    if (sqr_br === 0 && crl_br === 0 && brk === 0) {
+    if (sqr_br === 0 && crl_br === 0 && brk === 0 && count === input.length) {
         return true
     } else {
         return false
